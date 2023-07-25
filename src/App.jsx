17f29276b2c7f0550391ff219aeda264acb070e0
impronen <impronen@gmail.com>
2023-07-25 15:43:28 +0300

    add css variables

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,12 @@
 import { useState } from "react";
+import Greeting from "./components/Greeting";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  function Greeting() {
-    return (
-      <h1>&quot;I swear by my pretty floral bonnet, I will end you.&quot;</h1>
-    );
-  }
-
   return (
     <>
-      <div></div>
       <h1>Antero&apos;s Portfolio</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>This is for me to show what I can do.</p>
         <Greeting />
       </div>
     </>
---
+++ src/components/Greeting.jsx
@@ -0,0 +1,5 @@
+export default function Greeting() {
+  return (
+    <h1>&quot;I swear by my pretty floral bonnet, I will end you.&quot;</h1>
+  );
+}
