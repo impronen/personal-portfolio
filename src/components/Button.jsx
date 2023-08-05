f79877b74cdcbf46f5e3f9adec3af027eb076f70
impronen <impronen@gmail.com>
2023-08-05 18:58:14 +0300

    scroll to projects working

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,9 @@
 export default function Button(props) {
   return (
     <>
-      <button className={props.className}>projects</button>
+      <button className={props.className} onClick={props.onClick}>
+        projects
+      </button>
     </>
   );
 }
