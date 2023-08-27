bb71a7ae08e4d3523c541047d24a4b28c45aea80
impronen <impronen@gmail.com>
2023-08-27 21:40:36 +0300

    add project picture + features

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,12 +1,29 @@
 import Button from "./Button";
 
-export default function Project({ project, description, github, live }) {
+export default function Project({
+  project,
+  description,
+  github,
+  live,
+  image,
+  features,
+}) {
   return (
     <div className="project">
-      <h3 className="projectHeading">{project}</h3>
-      <p className="projectDescription">{description}</p>
-      <Button text="github" url={github} className="projectButton" />
-      <Button text="live" url={live} className="projectButton" />
+      <div className="leftWrapper">
+        <img src={image} alt={project} className="projectImage" />
+      </div>
+      <div className="rightWrapper">
+        <h3 className="projectHeading">{project}</h3>
+        <p className="projectDescription">{description}</p>
+        <ul className="featureList">
+          {features.map((feature, index) => (
+            <li key={index}>{feature}</li>
+          ))}
+        </ul>
+        <Button text="github" url={github} className="projectButton" />
+        <Button text="live" url={live} className="projectButton" />
+      </div>
     </div>
   );
 }
--- src/data/projectData.js
+++ src/data/projectData.js
@@ -2,25 +2,52 @@ export const projectData = [
   {
     project: "taskLister",
     description: "A todo list app made with vanilla JavaScript",
+    features: [
+      "OOP with class syntax",
+      "applied MVC pattern",
+      "localstorage for permanence between sessions",
+      "all elements created with vanilla DOM methods",
+    ],
     github: "https://github.com/impronen/odin-todo",
     live: "https://impronen.github.io/odin-todo/",
+    image: "../src/assets/images/todo.png",
   },
   {
     project: "Weather thingy",
-    description: "Responsive weather app with geolocation",
+    description: "Fully responsive weather app",
+    features: [
+      "sleek UI",
+      "geolocation or manual search",
+      "icon varies between night and day variants",
+      "fully responsive",
+    ],
     github: "https://github.com/impronen/odin-weather",
     live: "https://impronen.github.io/odin-weather/",
+    image: "../src/assets/images/weather.png",
   },
   {
     project: "Battleship",
     description: "A classic game of Battleship made with TDD",
+    features: [
+      "built with test driven development",
+      "play against computer AI",
+      "class syntax with vanilla JS",
+    ],
     github: "https://github.com/impronen/battleship",
     live: "https://impronen.github.io/battleship/",
+    image: "../src/assets/images/battleship.png",
   },
   {
     project: "CV App",
     description: "A responsive React app to create a CV",
+    features: [
+      "live rendering of general data",
+      "ability to add several previous jobs and degrees",
+      "fully responsive",
+      "simple, elegant ui",
+    ],
     github: "https://github.com/impronen/cv-app",
     live: "https://cv-app-rho.vercel.app/",
+    image: "../src/assets/images/cvapp.png",
   },
 ];
