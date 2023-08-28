93cae20ab00a0f1ae43da0dd801f9472095ddd6f
impronen <impronen@gmail.com>
2023-08-28 20:31:17 +0300

    change styling of project cards

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -17,12 +17,15 @@ export default function Project({
         <h3 className="projectHeading">{project}</h3>
         <p className="projectDescription">{description}</p>
         <ul className="featureList">
+          <h4>features</h4>
           {features.map((feature, index) => (
             <li key={index}>{feature}</li>
           ))}
         </ul>
-        <Button text="github" url={github} className="projectButton" />
-        <Button text="live" url={live} className="projectButton" />
+        <div className="buttonWrapper">
+          <Button text="github" url={github} className="projectButton" />
+          <Button text="live" url={live} className="projectButton" />
+        </div>
       </div>
     </div>
   );
